7111808b06ce7b8e87c93056e8c0dd65f1d6e9f2
garvita2003 <102051676+garvita2003@users.noreply.github.com>
2024-04-15 00:17:27 +0530

    Add files via upload

---
+++ config/config.js
@@ -0,0 +1,11 @@
+const sessionSecret = "blogsessionsecret";
+
+const emailUser = ""; //add smtp email and pass
+const emailPassword = "";
+
+
+module.exports = {
+    sessionSecret,
+    emailUser,
+    emailPassword
+}
